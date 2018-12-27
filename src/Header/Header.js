7291367c4da322278082d5866f6b39f890a92b77
m.darvishnejad <majid>
2018-12-27 11:55:01 +0330

    Header update and put a star grey to it

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,20 +1,51 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, Image} from 'react-native';
+import {View, Text, StyleSheet, Image, blurRadius} from 'react-native';
 
 export default class Header extends Component {
     render() {
         return (
             <View style={styles.className}>
-                <View style={{flex:1}}>
+                <View style={{flex: 1}}>
                     <Image
                         source={require('../image/Winter.jpg')}
                         style={styles.imageStyle}
                     />
                 </View>
                 <View style={styles.snow}>
-                    <View><Text>Snow Mountain</Text></View>
-                    <View><Text>Stands at the foot of the mountain , enjoy the most beautiful scenery in the world...</Text></View>
-                    <View><Text>kkk</Text></View>
+                    <View>
+                        <Text style={{color: 'white', fontSize: 30, fontWeight: 'bold'}}>Snow Mountain</Text>
+                    </View>
+                    <View>
+                        <Text style={{color:'white'}}>Stands at the foot of the mountain , enjoy the most beautiful scenery in the
+                            world...</Text>
+                    </View>
+                    <View style={styles.starStyle}>
+                        <View style={{flexDirection: 'row'}}>
+                            <Image
+                                source={require('../image/star.png')}
+                                style={styles.imageStarStyle}
+                            />
+                            <Image
+                                source={require('../image/star.png')}
+                                style={styles.imageStarStyle}
+                            />
+                            <Image
+                                source={require('../image/star.png')}
+                                style={styles.imageStarStyle}
+                            />
+                            <Image
+                                source={require('../image/star.png')}
+                                style={styles.imageStarStyle}
+                            />
+                            <Image
+                                source={require('../image/star-grey-hi.png')}
+                                style={[styles.imageStarStyle, {width: 16, height: 16}]}
+                            />
+                        </View>
+                        <View style={styles.locationButton}>
+                            <Text>kkk</Text>
+                        </View>
+                    </View>
                 </View>
             </View>
         );
@@ -29,11 +60,26 @@ const styles = StyleSheet.create({
         width: 420,
         height: 360,
     },
-    snow:{
-        flex:.7,
-        borderColor:'red',
+    snow: {
+        flex: .7,
+        borderColor: 'red',
         borderWidth: 5,
-        marginLeft: 50
+        marginLeft: 50,
+        justifyContent: 'space-evenly',
+        paddingLeft: 20
+    },
+    starStyle: {
+        backgroundColor: 'red',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        
+    },
+    imageStarStyle: {
+        marginLeft: 4
+    },
+    locationButton:{
+        marginRight: 30,
+        
     }
     
 });
